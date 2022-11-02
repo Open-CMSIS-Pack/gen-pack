cd3131c4b3e568af64e582ba52d9093172ef1afc
Jonatan Antoni <jonatan.antoni@arm.com>
2022-11-02 13:37:24 +0100

    Enhance git generated changelog
    Retain the changelog in PDSC file template for devdrop versions.

--- test/tests_pdsc.sh
+++ test/tests_pdsc.sh
@@ -4,10 +4,15 @@ shopt -s expand_aliases
 
 git_changelog_mock() {
 
+  echo "<!--"
+  for f in "$@"; do
+    echo "'$f'"
+  done;
+  echo "-->"
   cat <<EOF
 <releases>
   <release version="1.2.3">
-    Unit test change log
+    Unit test change log:
   </release>
 </releases>
 EOF
@@ -21,14 +26,14 @@ alias git_changelog='git_changelog_mock'
 setUp() {
   TESTDIR="${SHUNIT_TMPDIR}/${_shunit_test_}"
   mkdir -p "${TESTDIR}"
-  pushd "${TESTDIR}" >/dev/null  
+  pushd "${TESTDIR}" >/dev/null
 }
 
 test_locate_pdsc_auto() {
   touch "ARM.GenPack.pdsc"
 
   local pdsc=$(locate_pdsc)
-  
+
   assertEquals "$(realpath "$(pwd)/ARM.GenPack.pdsc")" "$pdsc"
 }
 
@@ -37,7 +42,7 @@ test_locate_pdsc_specific() {
   touch "ARM.GenPack2.pdsc"
 
   local pdsc=$(locate_pdsc "ARM.GenPack.pdsc")
-  
+
   assertEquals "$(realpath "$(pwd)/ARM.GenPack.pdsc")" "$pdsc"
 }
 
@@ -62,30 +67,73 @@ test_pdsc_update_releases() {
   <license>LICENSE</license>
 
   <releases>
-    <release version="0.0.0">
-      Active development ...
-    </release>
+    <release version="0.0.0"/>
   </releases>
-  
+
   <conditions>
     ...
   </conditions>
-  
+
   <components>
    ...
   </components>
-  
+
   ...
 </package>
 EOF
 
   mkdir -p output
-    
+
   pdsc_update_releases "ARM.GenPack.pdsc" "output/ARM.GenPack.pdsc" "v"
-  
+
   assertTrue "[ -f output/ARM.GenPack.pdsc ]"
-  
+
+  cat "output/ARM.GenPack.pdsc"
   local pdsc=$(cat "output/ARM.GenPack.pdsc")
+  assertContains    "${pdsc}"  "    <release version=\"1.2.3\">"
+  assertNotContains "${pdsc}"  "<release version=\"0.0.0\">"
+}
+
+test_pdsc_update_releases_with_devlog() {
+  cat > "ARM.GenPack.pdsc" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<package schemaVersion="1.7.7" xmlns:xs="http://www.w3.org/2001/XMLSchema-instance" xs:noNamespaceSchemaLocation="https://url.to/schema/PACK.xsd">
+  <vendor>ARM</vendor>
+  <name>GenPack</name>
+  <description>Test pack for GenPack library</description>
+  <url>http://www.keil.com/pack/</url>
+  <license>LICENSE</license>
+
+  <releases>
+    <release version="0.0.0">
+      Active development...
+      - Dev change log 1
+      - Dev change log 2
+    </release>
+  </releases>
+
+  <conditions>
+    ...
+  </conditions>
+
+  <components>
+   ...
+  </components>
+
+  ...
+</package>
+EOF
+
+  mkdir -p output
+
+  pdsc_update_releases "ARM.GenPack.pdsc" "output/ARM.GenPack.pdsc" "v"
+
+  assertTrue "[ -f output/ARM.GenPack.pdsc ]"
+
+  local pdsc=$(cat "output/ARM.GenPack.pdsc")
+  assertContains    "${pdsc}"  "Active development..."
+  assertContains    "${pdsc}"  "- Dev change log 1"
+  assertContains    "${pdsc}"  "- Dev change log 2"
   assertContains    "${pdsc}"  "    <release version=\"1.2.3\">"
   assertNotContains "${pdsc}"  "<release version=\"0.0.0\">"
 }
