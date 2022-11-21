421fcc9e3ed774f60ee1a3b1f5bd2a617bd19846
Jonatan Antoni <jonatan.antoni@arm.com>
2022-11-21 16:45:43 +0100

    Fix version sorting in git_changelog - Updated integration tests - Added integration test with pre-release version tag

--- test/test_integ_default/gen_pack.sh
+++ test/test_integ_default/gen_pack.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Version: 2.0
-# Date: 2022-07-28
+# Version: 2.4
+# Date: 2022-11-21
 # This bash script generates a CMSIS Software Pack:
 #
 
 set -o pipefail
 
 # Set version of gen pack library
-REQUIRED_GEN_PACK_LIB="0.1.0"
+REQUIRED_GEN_PACK_LIB=""
 
 # Set default command line arguments
 DEFAULT_ARGS=()
@@ -28,17 +28,46 @@ PACK_BASE_FILES="
 
 # Specify file names to be deleted from pack build directory
 PACK_DELETE_FILES="
-  doc/test.dxy
+    doc/test.dxy
 "
 
 # Specify patches to be applied
 PACK_PATCH_FILES="
-  test.patch
+    test.patch
 "
+
+# Specify addition argument to packchk
+PACKCHK_ARGS=()
+
+# Specify additional dependencies for packchk
+PACKCHK_DEPS=""
+
+# Optional: restrict fallback modes for changelog generation
+# Default: full
+# Values:
+# - full      Tag annotations, release descriptions, or commit messages (in order)
+# - release   Tag annotations, or release descriptions (in order)
+# - tag       Tag annotations only
+PACK_CHANGELOG_MODE="full"
+
+# custom pre-processing steps
+function preprocess() {
+  # add custom steps here to be executed
+  # before populating the pack build folder
+  return 0
+}
+
+# custom post-processing steps
+function postprocess() {
+  # add custom steps here to be executed
+  # after populating the pack build folder
+  # but before archiving the pack into output folder
+  return 0
+}
 
 ############ DO NOT EDIT BELOW ###########
 
-. "$(dirname "$0")/../../gen-pack"
+. "${GEN_PACK_LIB}/gen-pack"
 gen_pack "${DEFAULT_ARGS[@]}" "$@"
 
 exit 0
