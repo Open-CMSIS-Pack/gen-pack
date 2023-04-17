ad43fabbccf742e444bfb03c41f274eb9efab684
Jonatan Antoni <jonatan.antoni@arm.com>
2023-04-17 18:04:46 +0200

    Enhance documentation and error reporting

--- test/tests_pdsc.sh
+++ test/tests_pdsc.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Open-CMSIS-Pack gen-pack Bash library
+#
+# Copyright (c) 2022-2023 Arm Limited. All rights reserved.
+#
+# Provided as-is without warranty under Apache 2.0 License
+# SPDX-License-Identifier: Apache-2.0
+#
 
 shopt -s expand_aliases
 
