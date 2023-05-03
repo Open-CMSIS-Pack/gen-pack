55ac878371a08fcca67fa6f5e9ee32d703b7095c
Jonatan Antoni <jonatan.antoni@arm.com>
2023-05-03 11:51:42 +0200

    Keep existing release history in pdsc.
    *NOTE* The git_changelog_pdsc ommits <releases> tag now!

--- test/tests_pdsc.sh
+++ test/tests_pdsc.sh
@@ -18,11 +18,9 @@ git_changelog_mock() {
   done;
   echo "-->"
   cat <<EOF
-<releases>
-  <release version="1.2.3">
-    Unit test change log:
-  </release>
-</releases>
+<release version="1.2.3">
+  Unit test change log:
+</release>
 EOF
   return 0
 }
@@ -76,10 +74,15 @@ test_pdsc_release_desc() {
   <license>LICENSE</license>
 
   <releases>
-    <release version="0.0.0">
+    <release version="2.0.0">
       Active development...
       - Dev change log 1
       - Dev change log 2
+    </release>
+    <release version="1.0.0" date="2023-05-03">
+      Release 1.0.0
+      - Change log 1
+      - Change log 2
     </release>
   </releases>
 
@@ -134,6 +137,88 @@ EOF
   assertContains "${desc}" "No release description found in ARM.GenPack.pdsc!"
 }
 
+test_pdsc_release_desc_nodev() {
+    cat > "ARM.GenPack.pdsc" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<package schemaVersion="1.7.7" xmlns:xs="http://www.w3.org/2001/XMLSchema-instance" xs:noNamespaceSchemaLocation="https://url.to/schema/PACK.xsd">
+  <vendor>ARM</vendor>
+  <name>GenPack</name>
+  <description>Test pack for GenPack library</description>
+  <url>http://www.keil.com/pack/</url>
+  <license>LICENSE</license>
+
+  <releases>
+    <release version="1.0.0" date="2023-05-03">
+      Release 1.0.0
+      - Change log 1
+      - Change log 2
+    </release>
+  </releases>
+
+  <conditions>
+    ...
+  </conditions>
+
+  <components>
+   ...
+  </components>
+
+  ...
+</package>
+EOF
+
+  desc=$(pdsc_release_desc "ARM.GenPack.pdsc" 2>&1)
+  assertNotEquals 0 $?
+  assertContains "${desc}" "No release description found in ARM.GenPack.pdsc!"
+}
+
+
+test_pdsc_release_desc_exist() {
+    cat > "ARM.GenPack.pdsc" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<package schemaVersion="1.7.7" xmlns:xs="http://www.w3.org/2001/XMLSchema-instance" xs:noNamespaceSchemaLocation="https://url.to/schema/PACK.xsd">
+  <vendor>ARM</vendor>
+  <name>GenPack</name>
+  <description>Test pack for GenPack library</description>
+  <url>http://www.keil.com/pack/</url>
+  <license>LICENSE</license>
+
+  <releases>
+    <release version="6.0.0">
+      Active development...
+      - Dev change log 1
+      - Dev change log 2
+    </release>
+    <release version="5.9.0" date="2022-05-02">
+      CMSIS-Core(M): 5.6.0
+      CMSIS-DSP: 1.10.0 (see revision history for details)
+    </release>
+    <release version="5.8.0" date="2021-06-24">
+      CMSIS-Core(M): 5.5.0 (see revision history for details)
+      CMSIS-Core(A): 1.2.1 (see revision history for details)
+    </release>
+  </releases>
+
+  <conditions>
+    ...
+  </conditions>
+
+  <components>
+   ...
+  </components>
+
+  ...
+</package>
+EOF
+
+  desc=$(pdsc_release_desc "ARM.GenPack.pdsc")
+
+  assertEquals 0 $?
+  assertContains    "${desc}"  "Active development..."
+  assertContains    "${desc}"  "- Dev change log 1"
+  assertContains    "${desc}"  "- Dev change log 2"
+}
+
 test_pdsc_update_releases() {
   cat > "ARM.GenPack.pdsc" <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -166,7 +251,6 @@ EOF
 
   assertTrue "[ -f output/ARM.GenPack.pdsc ]"
 
-  cat "output/ARM.GenPack.pdsc"
   local pdsc=$(cat "output/ARM.GenPack.pdsc")
   assertContains    "${pdsc}"  "    <release version=\"1.2.3\">"
   assertNotContains "${pdsc}"  "<release version=\"0.0.0\">"
@@ -215,5 +299,105 @@ EOF
   assertContains    "${pdsc}"  "    <release version=\"1.2.3\">"
   assertNotContains "${pdsc}"  "<release version=\"0.0.0\">"
 }
+
+test_pdsc_update_releases_with_exist() {
+  cat > "ARM.GenPack.pdsc" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<package schemaVersion="1.7.7" xmlns:xs="http://www.w3.org/2001/XMLSchema-instance" xs:noNamespaceSchemaLocation="https://url.to/schema/PACK.xsd">
+  <vendor>ARM</vendor>
+  <name>GenPack</name>
+  <description>Test pack for GenPack library</description>
+  <url>http://www.keil.com/pack/</url>
+  <license>LICENSE</license>
+
+  <releases>
+    <release version="6.0.0">
+      Active development...
+      - Dev change log 1
+      - Dev change log 2
+    </release>
+    <release version="5.9.0" date="2022-05-02">
+      CMSIS-Core(M): 5.6.0
+      CMSIS-DSP: 1.10.0 (see revision history for details)
+    </release>
+    <release version="5.8.0" date="2021-06-24">
+      CMSIS-Core(M): 5.5.0 (see revision history for details)
+      CMSIS-Core(A): 1.2.1 (see revision history for details)
+    </release>
+  </releases>
+
+  <conditions>
+    ...
+  </conditions>
+
+  <components>
+   ...
+  </components>
+
+  ...
+</package>
+EOF
+
+  mkdir -p output
+
+  pdsc_update_releases "ARM.GenPack.pdsc" "output/ARM.GenPack.pdsc" "v"
+
+  assertTrue "[ -f output/ARM.GenPack.pdsc ]"
+
+  local pdsc=$(cat "output/ARM.GenPack.pdsc")
+  assertContains    "${pdsc}"  "Active development..."
+  assertContains    "${pdsc}"  "- Dev change log 1"
+  assertContains    "${pdsc}"  "- Dev change log 2"
+  assertContains    "${pdsc}"  "    <release version=\"1.2.3\">"
+  assertNotContains "${pdsc}"  "<release version=\"6.0.0\">"
+  assertContains    "${pdsc}"  "    <release version=\"5.9.0\" date=\"2022-05-02\">"
+  assertContains    "${pdsc}"  "    <release version=\"5.8.0\" date=\"2021-06-24\">"
+}
+
+test_pdsc_update_releases_with_exist_nodev() {
+  cat > "ARM.GenPack.pdsc" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<package schemaVersion="1.7.7" xmlns:xs="http://www.w3.org/2001/XMLSchema-instance" xs:noNamespaceSchemaLocation="https://url.to/schema/PACK.xsd">
+  <vendor>ARM</vendor>
+  <name>GenPack</name>
+  <description>Test pack for GenPack library</description>
+  <url>http://www.keil.com/pack/</url>
+  <license>LICENSE</license>
+
+  <releases>
+    <release version="5.9.0" date="2022-05-02">
+      CMSIS-Core(M): 5.6.0
+      CMSIS-DSP: 1.10.0 (see revision history for details)
+    </release>
+    <release version="5.8.0" date="2021-06-24">
+      CMSIS-Core(M): 5.5.0 (see revision history for details)
+      CMSIS-Core(A): 1.2.1 (see revision history for details)
+    </release>
+  </releases>
+
+  <conditions>
+    ...
+  </conditions>
+
+  <components>
+   ...
+  </components>
+
+  ...
+</package>
+EOF
+
+  mkdir -p output
+
+  pdsc_update_releases "ARM.GenPack.pdsc" "output/ARM.GenPack.pdsc" "v"
+
+  assertTrue "[ -f output/ARM.GenPack.pdsc ]"
+
+  local pdsc=$(cat "output/ARM.GenPack.pdsc")
+  assertContains "${pdsc}"  "    <release version=\"1.2.3\">"
+  assertContains "${pdsc}"  "    <release version=\"5.9.0\" date=\"2022-05-02\">"
+  assertContains "${pdsc}"  "    <release version=\"5.8.0\" date=\"2021-06-24\">"
+}
+
 
 . "$(dirname "$0")/shunit2/shunit2"
